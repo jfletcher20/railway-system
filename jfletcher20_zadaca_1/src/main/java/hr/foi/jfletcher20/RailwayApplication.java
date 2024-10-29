ed698c3724169f97dc042b76047a0b486a075c65
Joshua Fletcher <jfletcher20@student.foi.hr>
2024-10-29 18:01:33 +0100

    Initial project preparation.

---
+++ jfletcher20_zadaca_1/src/main/java/hr/foi/jfletcher20/RailwayApplication.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package hr.foi.jfletcher20;
+
+/**
+ * 
+ */
+public class RailwayApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world~!");
+	}
+
+}
