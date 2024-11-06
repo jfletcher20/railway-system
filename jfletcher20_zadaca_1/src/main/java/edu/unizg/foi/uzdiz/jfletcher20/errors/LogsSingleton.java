9f46c944af896b5eda8c3f5591c327baff00a51b
jfletcher20 <jfletcher20>
2024-11-06 12:09:33 +0100

    Created logs singleton.

--- jfletcher20_zadaca_1/src/main/java/edu/unizg/foi/uzdiz/jfletcher20/errors/LogsSingleton.java
+++ jfletcher20_zadaca_1/src/main/java/edu/unizg/foi/uzdiz/jfletcher20/errors/LogsSingleton.java
@@ -1,40 +1,45 @@
 package edu.unizg.foi.uzdiz.jfletcher20.errors;
 
 public class LogsSingleton {
-  
+
   static private volatile LogsSingleton instance = new LogsSingleton();
   private int errorCount = 0;
+  public boolean logWarnings = false, logInfo = false;
 
   private LogsSingleton() {}
-  
+
   public static LogsSingleton getInstance() {
     return instance;
   }
-  
+
   public void logError(String message) {
-    System.out.println(" [!] <" + errorCount + "> Error: " + message);
+    System.out.println(" [!] <e" + errorCount + "> Error: " + message);
     errorCount++;
   }
-  
+
   public void logError(int row, String message) {
-    System.out.println(" [!] <" + errorCount + ":r" + row + "> Error: " + message);
+    System.out.println(" [!] <e" + errorCount + ":r" + row + "> Error: " + message);
     errorCount++;
   }
-  
+
   public void logWarning(String message) {
-    System.out.println(" [-] Warning: " + message);
+    if (logWarnings)
+      System.out.println("  [-] Warning: " + message);
   }
-  
+
   public void logWarning(int row, String message) {
-    System.out.println(" [-] Warning: Redak " + row + ": " + message);
+    if (logWarnings)
+      System.out.println("  [-] <w" + row + "> Warning: " + message);
   }
-  
+
   public void logInfo(String message) {
-    System.out.println(" [?] Info: " + message);
+    if (logInfo)
+      System.out.println("  [?] Info: " + message);
   }
-  
+
   public void logInfo(int row, String message) {
-    System.out.println(" [?] Info: Redak " + row + ": " + message);
+    if (logInfo)
+      System.out.println("  [?] <r" + row + "> Info: " + message);
   }
 
 }
